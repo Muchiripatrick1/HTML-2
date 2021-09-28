69eebf6b6310703a137e594ae181bf18304e81b2
patrick muchiri <muchiripatrick86@gmail.com>
2021-09-28 12:18:59 +0300

    second commit

---
+++ touch discount.js
@@ -0,0 +1,4 @@
+const hasStudentCard = false; // Assign false to the constant
+if (hasStudentCard === true) { // If you bring your student ID
+  console.log("Student discount applied"); // Not output because it does not meet the conditions
+}
